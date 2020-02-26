6ff97ac591d236aab22f320cfcb10a13ee9c9c94
ShamerD <shomax31@gmail.com>
2020-02-27 00:05:58 +0300

    Added time information; images now save

--- The_PAM_Clustering/test.py
+++ The_PAM_Clustering/test.py
@@ -1,22 +1,36 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import os
 from glob import glob
 from PIL import Image
 from PAM import PAM
+from time import time
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
+cwd = os.getcwd()
+new_test_dir = os.path.join(cwd, "imgs_clustered")
+if not os.path.exists(new_test_dir):
+    os.mkdir(new_test_dir)  # create dir for test results
+
 for img_path in glob(os.path.join(dir_path, 'imgs', '*')):  # get imgs
     img = np.array(Image.open(img_path))[:, :, :3].astype('int64')
     file_name = os.path.basename(img_path)
     img_vectorised = img.reshape((-1, 3))  # get (n_pixels, 3)
 
+    start = time()
     c, C, totalDist = PAM(img_vectorised, 3)  # 3 clusters, dist - manhattan
+    pam_time = time() - start
+    print("PAM executed in %.6f" % pam_time)
 
     img_new = np.zeros_like(img_vectorised)
     for i in range(img_new.shape[0]):
         img_new[i] = img_vectorised[C[i]]
-    img_new = img_new.reshape(img.shape)  # clustered image
+    img_new = img_new.reshape(img.shape).astype('uint8')  # clustered image
+
+    img_name = "clustered_" + os.path.split(img_path)[-1]
+    im = Image.fromarray(img_new).convert('RGB')
+    im.save(os.path.join(os.path.split(new_test_dir)[-1], img_name))
 
     fig = plt.figure(figsize=(16, 8))
     fig.add_subplot(1, 2, 1)
