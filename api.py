2446d89017054f8e45f340207a53fbebbfa032c6
ShamerD <shomax31@gmail.com>
2020-04-08 17:28:06 +0300

    added database usage scratch

--- api.py
+++ api.py
@@ -1,12 +1,62 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 from AprioriDP.AprioriDP import apriori
 from The_PAM_Clustering.PAM import PAM
 
+# Flask aplication
 app = Flask(__name__)
+
+# SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db\\experiments.db'
+db = SQLAlchemy(app)
+
+# Flask_restful
 api = Api(app)
 
 experiments = {}
+
+
+class DBExperiment(db.Model):
+    __tablename__ = 'experiments'
+    id = db.Column(db.Integer, primary_key=True)
+    params = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return "<Experiment %d with parameters %s>" % (self.id, self.params)
+
+
+class DBClusterResult(db.Model):
+    __tablename__ = 'cluster_results'
+    id = db.Column(db.Integer, primary_key=True)
+
+    exp_id = db.Column(db.Integer,
+                       db.ForeignKey('experiments.id'), nullable=False)
+    experiments = db.relationship('DBExperiment',
+                                  backref=db.backref('clusters', lazy=True))
+
+    # stud_id = db.Column(db.Integer, db.ForeignKey('stud.id'), nullable=False)
+    # stud = db.relationship('Student')  # TODO db from other resource
+
+    cluster = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return '''<Cluster experiment %d shows
+                student %d is in cluster %d>''' % (self.exp_id,
+                                                   self.stud_id,
+                                                   self.cluster)
+
+
+class DBRuleResult(db.Model):
+    __tablename__ = 'rule_results'
+    id = db.Column(db.Integer, primary_key=True)
+    exp_id = db.Column(db.Integer,
+                       db.ForeignKey('experiments.id'), nullable=False)
+    experiments = db.relationship('DBExperiment',
+                                  backref=db.backref('rules', lazy=True))
+
+    rule = db.Column(db.Text, nullable=False)
 
 
 def check_if_exp_exists(exp_id):
@@ -87,4 +137,5 @@ api.add_resource(Experiment, '/experiments/<int:exp_id>')
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
